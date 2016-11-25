85eac0d5e3882f3d6ffd7c01e7ba12bc33f69f77
Jorn <j.c.rietveld-1@student.tudelft.nl>
2016-11-25 15:58:17 +0100

    Added scripts again and made palyable

---
+++ Assets/Scripts/CarInteract.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarInteract : PhilInteractable {
+
+	public override void Interact(GameObject player){
+
+		player.active = false;
+		//player.GetComponent<PhilMovement> ().enabled = false;
+		//player.GetComponent<MeshRenderer> ().enabled = false;
+		player.transform.parent = this.transform.FindChild ("Seat").transform;
+		player.transform.position = this.transform.FindChild ("Seat").transform.position;
+		//player.GetComponent<BoxCollider> ().enabled = false;
+		player.GetComponent<Rigidbody> ().useGravity = false;
+		this.GetComponent<CarControl> ().enabled = true;
+	}
+
+}
---
+++ Assets/Scripts/GiveHint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiveHint : PhilInteractable {
+    
+    public string[] dialogue;
+    public override void Interact(GameObject Interacted)
+    {
+        if (!PhilDialogue.Instance.dialoguePanel.activeSelf)
+        {
+            print("hint");
+            PhilDialogue.Instance.AddNewDialogue(dialogue);
+        }
+        else
+        {
+            PhilDialogue.Instance.ContinueDialogue();
+        }
+        
+    }
+}
