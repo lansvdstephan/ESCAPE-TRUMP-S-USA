2f91a5e83a012d58664774f7efae97c8741870d6
Lilian Dijkhoff <liliandijkhoff@hotmail.com>
2016-12-19 16:37:59 +0100

    made "hand" a child of the Obama's hand

--- Assets/Scripts/Background Controllers/PhilMovement.cs
+++ Assets/Scripts/Background Controllers/PhilMovement.cs
@@ -12,6 +12,7 @@ public class PhilMovement : MonoBehaviour {
     public Text healthText;
 
     private Rigidbody rb;
+    private GameObject hand;
 	private Animator anim;
     private Quaternion Rotation = Quaternion.LookRotation(new Vector3(0, 0, 1));
     private float viewRange = 1;
@@ -25,6 +26,7 @@ public class PhilMovement : MonoBehaviour {
     void Awake()
     {
         player = this.gameObject;
+        hand = this.transform.FindChild("Armature").FindChild("Bone").FindChild("handik.R").FindChild("handik.R_end").FindChild("Hand").gameObject;
     }
 
     void Start()
@@ -46,7 +48,7 @@ public class PhilMovement : MonoBehaviour {
         if (!PhilDialogue.Instance.dialoguePanel.activeSelf) {
             Move();
         }
-        if (player.transform.FindChild("Hand").childCount != 0)
+        if (hand.transform.childCount != 0)
         {
             if (pickedUp)
             {
@@ -127,15 +129,15 @@ public class PhilMovement : MonoBehaviour {
         // Dropping item if left shift is pressed else doing action if space is pressed
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            this.transform.FindChild("Hand").GetChild(0).GetComponent<CapsuleCollider>().enabled = true;
-            this.transform.FindChild("Hand").DetachChildren();
+           hand.transform.GetChild(0).GetComponent<CapsuleCollider>().enabled = true;
+           hand.transform.DetachChildren();
             InventorySystem.Instance.SwitchInventoryImange();
             InventorySystem.Instance.SwitchHandImage();
 
         }
         else if (Input.GetKeyUp("space"))
         {
-            if (this.transform.FindChild("Hand").GetChild(0).GetComponent<PickUpAble>().GetAction())
+            if (hand.transform.GetChild(0).GetComponent<PickUpAble>().GetAction())
             {
                 return;
             }
@@ -147,13 +149,13 @@ public class PhilMovement : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            if (player.transform.FindChild("Hand").childCount != 0) this.transform.FindChild("Hand").GetChild(0).GetComponent<PickUpAble>().PlaceItemInBackOfInventory(player);
+            if (hand.transform.childCount != 0) hand.transform.GetChild(0).GetComponent<PickUpAble>().PlaceItemInBackOfInventory(player);
             if (this.transform.FindChild("Inventory").childCount != 0) this.transform.FindChild("Inventory").GetChild(0).GetComponent<PickUpAble>().PlaceItemInHand(player);
             InventorySystem.Instance.SwitchInventoryImange();
         }
         else if (Input.GetKeyUp(KeyCode.X))
         {
-            if (player.transform.FindChild("Hand").childCount != 0) this.transform.FindChild("Hand").GetChild(0).GetComponent<PickUpAble>().PlaceItemInFrontOfInventory(player);
+            if (hand.transform.childCount != 0) hand.transform.GetChild(0).GetComponent<PickUpAble>().PlaceItemInFrontOfInventory(player);
             if (this.transform.FindChild("Inventory").childCount != 0) this.transform.FindChild("Inventory").GetChild(this.transform.FindChild("Inventory").childCount-1).GetComponent<PickUpAble>().PlaceItemInHand(player);
             InventorySystem.Instance.SwitchInventoryImange();
         }
