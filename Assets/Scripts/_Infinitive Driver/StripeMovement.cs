e27fec40ae8f7ecfd03c5940616472ca8fdf4801
Jorn <j.c.rietveld-1@student.tudelft.nl>
2016-12-20 10:33:35 +0100

    Reupdated the infinity driver scene

--- Assets/Scripts/_Infinitive Driver/StripeMovement.cs
+++ Assets/Scripts/_Infinitive Driver/StripeMovement.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StripeMovement : MonoBehaviour {
+public class StripeMovement : MonoBehaviour
+{
     public float speed = 1;
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector3 movement = new Vector3(0, 0, 1);
         movement = movement.normalized * speed * Time.deltaTime;
         this.transform.position = this.transform.position + movement;
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Player"))
+        {
+            speed = 0;
+        }
+    }
 }
