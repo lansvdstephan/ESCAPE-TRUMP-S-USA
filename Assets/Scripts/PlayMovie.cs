44be798cd44eb1b65ef3b8b577457e677af32efb
Peter Visser <pjdvisser@gmail.com>
2016-12-22 12:57:58 +0100

    Finished oval office

--- Assets/Scripts/PlayMovie.cs
+++ Assets/Scripts/PlayMovie.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayMovie1 : MonoBehaviour {
+public class PlayMovie : MonoBehaviour {
 
 	public MovieTexture movTexture;
 	public Animator alarm;
