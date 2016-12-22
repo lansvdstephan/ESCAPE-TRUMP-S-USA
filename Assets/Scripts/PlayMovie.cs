56da13f8574ae64ef8ea297668ad33f13e94d17e
Peter Visser <pjdvisser@gmail.com>
2016-12-22 11:59:28 +0100

    fixed oval office

---
+++ Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovie : MonoBehaviour {
+
+	public MovieTexture movTexture;
+	public Animator alarm;
+
+	private bool updateOn = true;
+
+	// Use this for initialization
+	void Start () {
+		movTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		if (updateOn) {
+			if (!movTexture.isPlaying){
+					TimerEnded ();
+				}
+			}
+		}
+
+		void TimerEnded () {
+			alarm.SetTrigger ("Alarm");
+			updateOn = false;
+		}
+	
+}
+
