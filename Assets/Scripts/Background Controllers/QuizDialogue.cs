06b1b70906d0837855da0aa9bb107429bd796aeb
Lilian Dijkhoff <liliandijkhoff@hotmail.com>
2016-12-22 13:56:25 +0100

    added script for quizdialogue

--- Assets/Scripts/Background Controllers/QuizDialogue.cs
+++ Assets/Scripts/Background Controllers/QuizDialogue.cs
@@ -9,17 +9,20 @@ public class QuizDialogue : MonoBehaviour
     public GameObject quizPanel;
     public string[] questions;
     public bool[] answers;
+    public int questionAmount;
 
     private List<string> questionLines;
     private List<bool> questionAnswers;
-    private Text trueFalseText;
-    private Text quizText;
+    private List<int> answeredQuestions;
+    private Text trueFalseText; //deze text zie je op je scherm 
+    private Text quizText; // deze text zie je op je scherm
     private int quizIndex;
+  
 
     void Awake()
     {
         quizText = quizPanel.transform.FindChild("Quiz Text").GetComponent<Text>();
-        trueFalseText = quizPanel.transform.FindChild("trueFalseText").GetComponent<Text>();
+        trueFalseText = quizPanel.transform.FindChild("TrueFalse Text").GetComponent<Text>();
         quizPanel.SetActive(false);
 
         if (Instance != null && Instance != this)
@@ -32,42 +35,58 @@ public class QuizDialogue : MonoBehaviour
         }
     }
 
-    public void StartQuiz()
+    public void StartQuiz() //aanroepen op spatie
     {
-        quizIndex = Random.Range(0, questions.Length);
+        Time.timeScale = 0.0f;
         questionLines = new List<string>(questions.Length);
-        questionLines.AddRange(questions);
+        questionLines.AddRange(questions); //kopieer questions naar de lijst 
         questionAnswers = new List<bool>(answers.Length);
-        questionAnswers.AddRange(answers);
-        CreateDialogue();
-    }
-
-    public void CreateDialogue()
-    {
-        print("started dialog");
-        quizText.text = questionLines[quizIndex];
+        questionAnswers.AddRange(answers); //kopieer answers naar de lijst 
+        answeredQuestions = new List<int>(questions.Length); //lijst is leeg 
+        quizIndex = Random.Range(0, questions.Length); 
+        answeredQuestions.Add(quizIndex);
+        trueFalseText.text = "True or False: ";
+        quizText.text = questionLines[quizIndex]; 
         quizPanel.SetActive(true);
-    }
-
-    public void CheckAnswer(bool ans)
-    {
         
     }
 
-    public void ContinueDialogue()
-    {
 
-        if (quizIndex < questionLines.Count - 1)
+    public void CheckAnswer(bool ans) // deze functie aanroepen on click 
+    {
+        if (questionAnswers[quizIndex] == ans) 
         {
-            quizIndex++;
-            quizText.text = questionLines[quizIndex];
-            print("Dialogue continue");
-        }
-        else
-        {
+            questionAmount++;
+            if (questionAmount == 5)
+            {
+                quizText.text = "You found the code: 1946";
+                if (Input.GetButtonUp("space"))
+                {
+                    Time.timeScale = 1.0f;
+                    quizPanel.SetActive(false);
+                }
+            }
+            else
+            {
+                NextQuestion(); 
+            }
+            
+        } else {
             quizPanel.SetActive(false);
-            print("Dialogue ended");
+            Time.timeScale = 1.0f;
+       }
+    }
+  
+
+    public void NextQuestion()
+    {
+        while (answeredQuestions.Contains(quizIndex))
+        {
+          quizIndex = Random.Range(0, questions.Length);
         }
+        answeredQuestions.Add(quizIndex);
+        quizText.text = questionLines[quizIndex];
+       
     }
 }
 
