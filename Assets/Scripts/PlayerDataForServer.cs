41818e300258d5c529889389a7d7c8d449407a66
Peter Visser <pjdvisser@gmail.com>
2016-12-23 11:23:34 +0100

    unity analytics added

--- Assets/Scripts/PlayerDataForServer.cs
+++ Assets/Scripts/PlayerDataForServer.cs
@@ -3,16 +3,35 @@ using System.Collections;
 
 public class PlayerDataForServer : MonoBehaviour {
 	public int counter;
+	public float timer;
+
 
 	// Use this for initialization
-	void Start () {
+	void awake () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		timer += Time.deltaTime;
 		if (Input.GetKeyUp ("space")) {
 			counter++;
 		}
+//		if (Application.loadedLevel == 2){
+//			Analytics.CustomEvent("First Level", new Dictionary<string, object>
+//				{
+//					{ "spaces", counter },
+//					{ "time",  timer    }
+//				});
+//		}
+//		if (Application.loadedLevel == 3){
+//			Analytics.CustomEvent("Second Level", new Dictionary<string, object>
+//				{
+//					{ "spaces", counter },
+//					{ "time",  timer    }
+//				});
+		}
 	}
-}
+
+
+				
