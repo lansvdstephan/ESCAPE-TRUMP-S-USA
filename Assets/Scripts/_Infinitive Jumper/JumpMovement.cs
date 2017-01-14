fc0b5ba561e8f306aa8c1fb3deb6139b9567c18b
JoepBom <joep_bom@live.nl>
2017-01-14 11:56:45 +0100

    First attempt of obama animation for Jumping level

--- Assets/Scripts/_Infinitive Jumper/JumpMovement.cs
+++ Assets/Scripts/_Infinitive Jumper/JumpMovement.cs
@@ -8,35 +8,46 @@ public class JumpMovement : MonoBehaviour {
     private int animWalkingHash = Animator.StringToHash("Walking");
     private int animTakeOffHash = Animator.StringToHash("Take Off");
     private int animLandingHash = Animator.StringToHash("On Ground");
+    private Quaternion Rotation;
 
     private Rigidbody rb;
-    private bool onGround = true;
+    public bool onGround = true;
 
     // Use this for initialization
     void Start () {
         Physics.gravity = Physics.gravity * 9f;
         rb = this.GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        Rotation = this.transform.rotation;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         float h = Input.GetAxis("Horizontal");
         Vector3 movement = new Vector3(h, 0, 0);
         movement = movement * speed * Time.deltaTime;
         this.transform.position = this.transform.position + movement;
+        if (movement == new Vector3(0, 0, 0))
+        {
+            rb.MoveRotation(Rotation);
+        }
+        else
+        {
+            Rotation = Quaternion.LookRotation(movement.normalized);
+            rb.MoveRotation(Rotation);
+        }
         if (h != 0)
         {
             anim.SetBool(animWalkingHash, true);
         }
-        if (Input.GetKeyDown("up")&& onGround)
+        if (Input.GetKeyDown("up") && onGround)
         {
             anim.SetTrigger(animTakeOffHash);
             rb.AddForce(Vector3.up * jumpForce);
         }
-        if (onGround)
-        {
-            anim.SetBool(animLandingHash, true);
-        }
+        anim.SetBool(animLandingHash, onGround);
+
     }
 
     void FixedUpdate()
