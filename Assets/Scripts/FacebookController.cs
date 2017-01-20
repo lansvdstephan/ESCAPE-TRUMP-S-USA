c675076512e98d75f08a0aae4dc99829aa1158ba
Lilian Dijkhoff <liliandijkhoff@hotmail.com>
2017-01-20 15:16:12 +0100

    merge

--- Assets/Scripts/FacebookController.cs
+++ Assets/Scripts/FacebookController.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FacebookController : MonoBehaviour
 {
     private string facebookID = "1327549797288752";
     private string facebookURL = "http://www.facebook.com/dialog/feed";
+    private string link = "https://escapetrumpsusa.itch.io/escape-trumps-usa";
+    private string name = "I played the game Escape Trump's USA!";
+    private string caption= "Try it out now!";
+    private string description = "I escaped Trump's USA with a high score of 1000! " + "Can you beat it?";
+    private string redirect = "http://facebook.com"; 
 
 
-    void ShareToFacebook(string linkParameter, string nameParameter, string captionParameter, string descriptionParameter, string pictureParameter, string redirectParameter)
+    public void ShareToFacebook()
     {
-        Application.OpenURL(facebookURL + "?app_id=" + facebookID + "&link=" + WWW.EscapeURL(linkParameter) + "&name=" + WWW.EscapeURL(nameParameter) + "&caption=" + WWW.EscapeURL(captionParameter) + "&description=" + WWW.EscapeURL(descriptionParameter) + "&picture=" + WWW.EscapeURL(pictureParameter) + "&redirect_uri=" + WWW.EscapeURL(redirectParameter));
+        Application.OpenURL(facebookURL + "?app_id=" + facebookID + "&link=" + WWW.EscapeURL(link) + "&name=" + WWW.EscapeURL(name) + "&caption=" + WWW.EscapeURL(caption) + "&description=" + WWW.EscapeURL(description) + "&redirect_uri=" + WWW.EscapeURL(redirect));
     }
 }
