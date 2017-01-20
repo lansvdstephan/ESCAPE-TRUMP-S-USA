c54692520db213286ff32fbe54291087f4fbadd7
Peter Visser <pjdvisser@gmail.com>
2017-01-20 17:15:43 +0100

    Trying to fix driver level

--- Assets/Scripts/_Infinitive Driver/Creator.cs
+++ Assets/Scripts/_Infinitive Driver/Creator.cs
@@ -8,10 +8,13 @@ public class Creator : MonoBehaviour {
     public GameObject player;
     public MultiDimensionGameObjectArray[] obj;
     public int maxPerStage;
+	public Animator cameraAnimator;
 
+	private bool endLevel;
     private int counter;
     private int i;
     private Vector3 offset;
+	private GameObject[] obstackleCreators;
 
     void Start()
     {
@@ -33,33 +36,37 @@ public class Creator : MonoBehaviour {
 
     public void OnTriggerExit(Collider other)
     {
-        if (counter != maxPerStage)
+		if (other.CompareTag("Plane"))
+		{
+			if (counter % 2 == 0) {
+				Instantiate (obj [i].gameObjectArr [Random.Range (0, obj [i].gameObjectArr.Length)], 
+					new Vector3 (0, this.gameObject.transform.position.y, this.gameObject.transform.position.z), Quaternion.identity);
+			} else {
+				Instantiate (obj [i].gameObjectArr [Random.Range (0, obj [i].gameObjectArr.Length)], 
+					new Vector3 (0, this.gameObject.transform.position.y + 0.01f, this.gameObject.transform.position.z), Quaternion.identity);
+			}
+			counter++;
+		}
+
+        if (counter == maxPerStage)
         {
-            if (other.CompareTag("Plane"))
-            {
-                if (counter % 2 == 0)
-                {
-                    Instantiate(obj[i].gameObjectArr[Random.Range(0, obj[i].gameObjectArr.Length)], 
-                        new Vector3(0, this.gameObject.transform.position.y, this.gameObject.transform.position.z), Quaternion.identity);
-                }
-                else
-                {
-                    Instantiate(obj[i].gameObjectArr[Random.Range(0, obj[i].gameObjectArr.Length)], 
-                        new Vector3(0, this.gameObject.transform.position.y + 0.01f, this.gameObject.transform.position.z), Quaternion.identity);
-                }
-                counter++;
-            }
-        }
-        else
-        {
-            if (i < obj.Length + 1)
+            if (i < obj.Length - 1)
             {
                 i++;
                 counter = 0;
             }
+
+			if (endLevel) {
+				EndGame ();
+			}
             else
             {
+				endLevel = true;
                 counter = 0;
+				obstackleCreators = GameObject.FindGameObjectsWithTag ("Obstackle Creator");
+				foreach (GameObject i in obstackleCreators){
+					i.SetActive (false);
+				}
                 print("This is the End.");
             }
         }
@@ -69,4 +76,13 @@ public class Creator : MonoBehaviour {
     {
         return i;
     }
+
+	public void EndGame(){
+		GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<CameraMovement1>().enabled = false;
+		cameraAnimator.SetBool ("End Game", true);
+	}
+
+	public void NextLevel() {
+		print ("next Level");
+	}
 }
