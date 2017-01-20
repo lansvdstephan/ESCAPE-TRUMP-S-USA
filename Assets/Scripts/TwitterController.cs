f2edb37852b13525186ad57e3be2bb29eb5670cf
Lilian Dijkhoff <liliandijkhoff@hotmail.com>
2017-01-20 11:27:16 +0100

    added twitter and facebookcontroller to share highscores

---
+++ Assets/Scripts/TwitterController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwitterController : MonoBehaviour {
+
+    private string twitterAddress = "http://twitter.com/intent/tweet"; 
+    private string tweetLanguage = "en";
+    private string textToDisplay = "test tweet to Unity";
+
+
+    void ShareToTwitter (string textToDisplay) //aanroepen op onClick()
+    {
+        Application.OpenURL(twitterAddress + "?text=" + WWW.EscapeURL(textToDisplay) + "&amp;lang="
++ WWW.EscapeURL(tweetLanguage));
+    }
+}
