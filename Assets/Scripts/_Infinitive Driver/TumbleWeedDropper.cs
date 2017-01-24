edc20f685ff9080c610ae692fbe030ae710db76c
DESKTOP-N045B2Q\Gebruiker <lansvdstephan@gmail.com>
2017-01-24 15:00:28 +0100

    under the wall improved

--- Assets/Scripts/_Infinitive Driver/TumbleWeedDropper.cs
+++ Assets/Scripts/_Infinitive Driver/TumbleWeedDropper.cs
@@ -8,12 +8,13 @@ public class TumbleWeedDropper : MonoBehaviour {
 	public float spawnMax = 15f;
 
 	void Start () {
-		Invoke("Spawn", Random.Range(spawnMin, spawnMax));
+        Spawn();
 	}
 
 	void Spawn()
 	{
 		Instantiate(tumbleWeed, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z + Random.Range(-33,33)), Quaternion.identity);
-		Invoke("Spawn", Random.Range(spawnMin, spawnMax));
+        Debug.Log("something");
+        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
 	}
 }
