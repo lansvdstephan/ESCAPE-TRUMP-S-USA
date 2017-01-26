ada6ab050c584deca4d9936ad6becc9f316db96f
Jorn <j.c.rietveld-1@student.tudelft.nl>
2017-01-26 13:59:12 +0100

    First step in making the conversation partner changeable.

--- Assets/Scripts/Background Controllers/PhilDialogue.cs
+++ Assets/Scripts/Background Controllers/PhilDialogue.cs
@@ -8,15 +8,20 @@ public class PhilDialogue : MonoBehaviour {
     public static PhilDialogue Instance { get; set; }
     public GameObject dialoguePanel;
     public bool Noanimation = false;
+    public Sprite joe;
 
     private List<string> dialogueLines;
+    private Image narrator;
     private Text dialogueText;
     private int dialogueIndex;
 
     void Awake ()
     {
         dialogueText = dialoguePanel.transform.FindChild("Dialogue Text").GetComponent<Text>();
-        dialoguePanel.SetActive(false);        
+        narrator = dialoguePanel.transform.FindChild("Image").GetComponent<Image>();
+        dialoguePanel.SetActive(false);
+
+        narrator.sprite = joe;
 
         if (Instance != null && Instance != this)
         {
@@ -33,9 +38,26 @@ public class PhilDialogue : MonoBehaviour {
         dialogueIndex = 0;
         dialogueLines = new List<string>(lines.Length);
         dialogueLines.AddRange(lines);
-        if(Noanimation)
-            Time.timeScale = 0f;
+        if(Noanimation) Time.timeScale = 0f;
         CreateDialogue();
+        narrator.sprite = joe;
+    }
+
+    public void AddNewDialogue(string[] lines, Sprite converstationPartner)
+    {
+        dialogueIndex = 0;
+        dialogueLines = new List<string>(lines.Length);
+        dialogueLines.AddRange(lines);
+        if (Noanimation) Time.timeScale = 0f;
+        CreateDialogue();
+        if (converstationPartner != null)
+        {
+            narrator.sprite = converstationPartner;
+        }
+        else
+        {
+            narrator.sprite = joe;
+        }
     }
 
     public void CreateDialogue()
--- Assets/Scripts/Interactable Objects/PhilInteractable.cs
+++ Assets/Scripts/Interactable Objects/PhilInteractable.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PhilInteractable : MonoBehaviour {
     public bool noAnimation = false;
+    public Sprite dialogueSprite;
 
     public virtual void Interact(GameObject player)
     {
