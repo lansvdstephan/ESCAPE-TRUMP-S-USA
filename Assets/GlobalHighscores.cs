c546aba5ff665b2cc5530242fd94c1cec4fee135
JoepBom <joep_bom@live.nl>
2017-01-26 11:39:23 +0100

    mickey

---
+++ Assets/GlobalHighscores.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadHighscores : MonoBehaviour {
+    public GameObject HighScorePanel;
+    public void StartGlobalHighscores()
+    {
+        HighScorePanel.GetComponent<HighScoreController>().startGetScores();
+    }
+
+    public void StartLocalHighscores()
+    {
+        HighScorePanel.GetComponent<HighScoreController>().startGetLocalScores();
+    }
+}
